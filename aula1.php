931f9dae200c4e9f93999b05ce8c19c923ec0828
Arthur Sant <artur.santana@austa.com.br>
2025-07-24 16:38:05 -0300

    funcionalidade excutada

---
+++ aula1.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Branch é a forma que o git separa as versões dos projetos;
+ * Quando um projeto é criado ele inicia na branch master, estamos trabalhando nela até este ponto do curso
+ * Geralmente cada nova feature de um projeto fica em uma branch separado;
+ * Após a finalização das alterações os brnchs são unidos para ter o código-fonte final;
+ */
