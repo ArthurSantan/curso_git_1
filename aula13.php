16628245bcfe52a3457c0fc0f194a6b20e5ca3f6
Arthur Sant <artur.santana@austa.com.br>
2025-07-24 15:55:15 -0300

    Enviando aruqivos

---
+++ aula13.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Com o comando git mv podemos renomear um arquivo;
+ * O mesmo também pode ser movido para outrapasta;
+ * E isso fará com que este novo arquivo seja monitorado pelo git;
+ * O arquivo anterior será excluido; 
+ */
