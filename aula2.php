af4ec56c180b15f54008b22e026f06c66be5423c
Arthur Sant <artur.santana@austa.com.br>
2025-07-24 16:29:50 -0300

    Novo arquivo

---
+++ aula2.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Criando e visualizando os branchs;
+ * 
+ * Para visualizar os branchs disponíveis basta digitar git branch;
+ * Para criar um branch você precisa utilizar o comando git branch <nome>;
+ * Estas duas operações são muito utilizados no dia a dia de um dev;
+ */
