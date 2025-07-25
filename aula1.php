93dc80b36b3fa890432792c37a54f3284b549b0f
Arthur Sant <artur.santana@austa.com.br>
2025-07-25 10:43:26 -0300

    editando

---
+++ aula1.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Encontrnado branches
+ * 
+ * Branches novos são criados a todo tempo e o seu git pode não estar mapeando eles;
+ * Com o comando git fetch você é atualizado de todos os branchs e tags que ainda não estão reconhecidos por você;
+ * Este comando é util para utilizar o branch de algum outro dev do time, por exemplo;
+ */
